9f55c775af37274a890b608fc10f90d56d41049b
Faiq <mfaiqn@gmail.com>
2021-12-07 14:58:50 +0700

    create task component

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <hello/>
-    <info/>
+  <div class="container">
+    <header>
+      <h1>Tugasku</h1>
+    </header>
+    <Task v-for="task in getTasks()" v-bind:key="task._id" v-bind:task="task" />
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello.vue'
-import Info from './components/Info.vue'
+import Vue from "vue";
+import Task from "./components/Task.vue";
 
 export default {
   components: {
-    Hello,
-    Info,
+    Task,
   },
-}
+  data() {
+    return {};
+  },
+  methods: {
+    getTasks() {
+      return [
+        {
+          _id: 1,
+          matkul: "Statistik dan Probabilitas",
+          deskripsi: "Hipotesis",
+          deadline: "2021/12/17",
+          pengumpulan: "ethol",
+          status: false,
+        },
+        {
+          _id: 2,
+          matkul: "Prak Konsep Jaringan",
+          deskripsi: "Static Routing",
+          deadline: "2021/12/30",
+          pengumpulan: "classroom",
+          status: true,
+        },
+        {
+          _id: 3,
+          matkul: "GIS",
+          deskripsi: "Geocoding",
+          deadline: "2021/12/19",
+          pengumpulan: "gdrive",
+          status: true,
+        },
+      ];
+    },
+  },
+};
 </script>
 
 <style>
-  body {
-    font-family: sans-serif;
-    padding: 10px;
-  }
 </style>
---
+++ imports/ui/components/Task.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="task-container">
+    <!-- <h3>To Do</h3> -->
+    <div
+      v-if="task.status == false"
+      class="todo"
+      style="background-color: gray"
+    >
+      <ul>
+        <li>Matkul: {{ this.task.matkul }}</li>
+        <li>Deskripsi: {{ this.task.deskripsi }}</li>
+        <li>Deadline: {{ this.task.deadline }}</li>
+        <li>Pengumpulan: {{ this.task.pengumpulan }}</li>
+        <li>Status: {{ this.task.status }}</li>
+      </ul>
+    </div>
+    <!-- <h3>Completed</h3> -->
+    <div
+      v-if="task.status == true"
+      class="complete"
+      style="background-color: green"
+    >
+      <ul>
+        <li>Matkul: {{ this.task.matkul }}</li>
+        <li>Deskripsi: {{ this.task.deskripsi }}</li>
+        <li>Deadline: {{ this.task.deadline }}</li>
+        <li>Pengumpulan: {{ this.task.pengumpulan }}</li>
+        <li>Status: {{ this.task.status }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["task"],
+  data() {
+    return {};
+  },
+};
+</script>
