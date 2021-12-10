377896c9a5dbcce473071b6354ff2b6849d81e4f
Faiq <mfaiqn@gmail.com>
2021-12-10 22:53:00 +0700

    use collection

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -5,7 +5,7 @@
     </header>
     <main>
       <!-- <Task v-for="task in getTasks()" v-bind:key="task._id" v-bind:task="task" /> -->
-      <Task v-bind:tasks="getTasks()" />
+      <Task v-bind:tasks="getTasks" />
     </main>
   </div>
 </template>
@@ -13,6 +13,7 @@
 <script>
 import Vue from "vue";
 import Task from "./components/Task.vue";
+import { TasksCollection } from "../api/TasksCollection";
 
 export default {
   components: {
@@ -21,48 +22,12 @@ export default {
   data() {
     return {};
   },
-  methods: {
+  methods: {},
+  meteor: {
     getTasks() {
-      return [
-        {
-          _id: 1,
-          matkul: "Statistik dan Probabilitas",
-          deskripsi: "Hipotesis",
-          deadline: "2021/12/17",
-          // pengumpulan: "https://ethol.pens.ac.id/mahasiswa/tugas-online",
-          pengumpulan: "ethol",
-          status: "todo",
-        },
-        {
-          _id: 2,
-          matkul: "Prak Konsep Jaringan",
-          deskripsi: "Static Routing",
-          deadline: "2021/12/30",
-          // pengumpulan: "https://classroom.google.com/u/0/c/MzkyOTEyNjIzMzg1/a/NDI5ODg2MDkyMzQ1/details",
-          pengumpulan: "classroom",
-          status: "todo",
-        },
-        {
-          _id: 3,
-          matkul: "GIS",
-          deskripsi: "Geocoding",
-          deadline: "2021/12/19",
-          // pengumpulan: "https://drive.google.com/drive/u/1/folders/1psLLNT-OauEGqWxWDMuXjkKYJuHP0gFz",
-          pengumpulan: "google drive",
-          status: "done",
-        },
-        {
-          _id: 4,
-          matkul: "B. Inggris",
-          deskripsi: "Video Resume",
-          deadline: "2021/12/22",
-          // pengumpulan: "https://drive.google.com/drive/u/1/folders/1psLLNT-OauEGqWxWDMuXjkKYJuHP0gFz",
-          pengumpulan: "google drive",
-          status: "progress",
-        },
-      ];
-    },
-  },
+      return TasksCollection.find({}).fetch();
+    }
+  }
 };
 </script>
 
--- server/main.js
+++ server/main.js
@@ -1,3 +1,40 @@
-import '../imports/api/fixtures'
-import '../imports/api/methods'
-import '../imports/api/publications'
+import { Meteor } from 'meteor/meteor';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+const insertTask = item => TasksCollection.insert(item);
+ 
+Meteor.startup(() => {
+  if (TasksCollection.find().count() === 0) {
+    const data = [
+      {
+          matkul: "Statistik dan Probabilitas",
+          deskripsi: "Hipotesis",
+          deadline: 2021-12-17,
+          pengumpulan: "ethol",
+          link: "https://ethol.pens.ac.id/mahasiswa/tugas-online",
+          status: "todo",
+          createdAt: new Date(),
+        },
+        {
+          matkul: "Prak Konsep Jaringan",
+          deskripsi: "Static Routing",
+          deadline: 2021-12-30,
+          pengumpulan: "classroom",
+          link: "https://classroom.google.com/u/0/c/MzkyOTEyNjIzMzg1/a/NDI5ODg2MDkyMzQ1/details",
+          status: "progress",
+          createdAt: new Date(),
+        },
+        {
+          matkul: "GIS",
+          deskripsi: "Geocoding",
+          deadline: 2021-12-19,
+          pengumpulan: "google drive",
+          link: "https://drive.google.com/drive/u/1/folders/1psLLNT-OauEGqWxWDMuXjkKYJuHP0gFz",
+          status: "done",
+          createdAt: new Date(),
+        }
+    ];
+    
+    data.forEach(insertTask);
+  }
+});
